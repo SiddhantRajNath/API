7c7ab9d50c4b9764fba0383be7bfa6e5bc6d68c1
siddhant <crossxerox@gmail.com>
2022-07-16 16:00:45 +0545

    Added user

--- Desktop/New folder/main.py
+++ Desktop/New folder/main.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 import database
 import models
 
-
 app = FastAPI()
 
 
@@ -11,3 +10,20 @@ app = FastAPI()
 def get_user(id: int, db: Session = Depends(database.get_db)):
     data = db.query(models.day2).filter(models.day2.Id == id).first()
     return {"data": data}
+   
+@app.post("/person")
+def add_person(username:str,
+password: str,
+Email: str,
+blog_id: int,
+db:Session = Depends(database.get_db)):
+    data=models.person(person_username=username,
+                     person_password =password, person_email=Email,blog_id=blog_id)
+    db.add(data)    
+    db.commit()
+
+# get method is use to readdata
+@app.get("/person/{id}")
+def get_person_by_id(id: int , db: Session = Depends(database.get_db)):
+  data = db.query(models.person).filter(models.person.person_id == id).first()
+  return {"status":200 , "message": "Data Found", "data": data}
--- Desktop/New folder/models.py
+++ Desktop/New folder/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, MetaData
+from lib2to3.pytree import Base
+from sqlalchemy import Column, Integer, MetaData, column
 from sqlalchemy.sql.sqltypes import NullType
 from sqlalchemy.ext.declarative import declarative_base
 base=declarative_base()
@@ -11,3 +12,12 @@ class day2(base):
     last_name=Column("Address",NullType)
     dob=Column("DOB",NullType)
 
+class person(base):
+    __tablename__="table_person"
+    person_id = Column(Integer,primary_key = True, index = True)
+    person_username = Column('username',NullType)
+    person_password = Column('password',NullType)
+    person_email = Column('email',NullType)
+    blog_id = Column('blog_id',NullType)
+
+    
